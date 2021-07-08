5785759bd689f03e91d20fff57a9449ac1eb9152
iambasanta <sangpangbasanta@gmail.com>
2021-07-08 09:28:37 +0545

    finish countdown-timer project

---
+++ countdown/app.js
@@ -0,0 +1,33 @@
+//get elemnts by id
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const newYear = "1 Jan 2022";
+
+function countdown() {
+  const newYearDate = new Date(newYear);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+//puts 0 in front, if value is  less than 10
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+//initial call
+countdown();
+setInterval(countdown, 1000);
