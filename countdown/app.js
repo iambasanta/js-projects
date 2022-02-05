bc85ad3a9af74da0f53a0474c449599fe59f5e7c
iambasanta <sangpangbasanta@gmail.com>
2022-02-05 21:15:47 +0545

    Update future date

--- countdown/app.js
+++ countdown/app.js
@@ -4,28 +4,28 @@ const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
-const newYear = "1 Jan 2022";
+const newYear = "1 Jan 3000";
 
 function countdown() {
-  const newYearDate = new Date(newYear);
-  const currentDate = new Date();
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
 
-  const totalSeconds = (newYearDate - currentDate) / 1000;
+    const totalSeconds = (newYearDate - currentDate) / 1000;
 
-  const days = Math.floor(totalSeconds / 3600 / 24);
-  const hours = Math.floor(totalSeconds / 3600) % 24;
-  const minutes = Math.floor(totalSeconds / 60) % 60;
-  const seconds = Math.floor(totalSeconds) % 60;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
 
-  daysEl.innerHTML = days;
-  hoursEl.innerHTML = formatTime(hours);
-  minutesEl.innerHTML = formatTime(minutes);
-  secondsEl.innerHTML = formatTime(seconds);
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
 }
 
 //puts 0 in front, if value is  less than 10
 function formatTime(time) {
-  return time < 10 ? `0${time}` : time;
+    return time < 10 ? `0${time}` : time;
 }
 
 //initial call
